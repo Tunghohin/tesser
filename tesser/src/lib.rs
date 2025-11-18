0b296a2ff4cd0e137e582299d5709edbc853e083
Ewig Midori <ewigmidori@gmail.com>
2025-11-18 11:14:34 +0000

    chore: tidy aggregate prelude exports

--- tesser/src/lib.rs
+++ tesser/src/lib.rs
@@ -17,18 +17,17 @@ pub use tesser_strategy_macros as strategy_macros;
 
 /// Convenience prelude to pull commonly used items into scope.
 pub mod prelude {
-    pub use tesser_core::*;
-    pub use tesser_events::*;
-    pub use tesser_strategy::*;
-    pub use tesser_strategy_macros::*;
-    pub use tesser_indicators::*;
-    pub use tesser_data::*;
-    pub use tesser_execution::*;
-    pub use tesser_portfolio::*;
-    pub use tesser_markets::*;
+    pub use tesser_backtester::{BacktestConfig, BacktestMode, BacktestReport, Backtester};
     pub use tesser_broker::*;
-    pub use tesser_backtester::*;
-    pub use tesser_paper::*;
     pub use tesser_bybit::*;
     pub use tesser_config::*;
+    pub use tesser_core::*;
+    pub use tesser_data::*;
+    pub use tesser_events::*;
+    pub use tesser_execution::*;
+    pub use tesser_indicators::*;
+    pub use tesser_markets::*;
+    pub use tesser_paper::*;
+    pub use tesser_portfolio::*;
+    pub use tesser_strategy::*;
 }
